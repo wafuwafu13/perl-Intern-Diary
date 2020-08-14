a9257c9dd317e726a4fc966118e64e824b3b68d4
wafuwafu13 <wafuwafu1313@gmail.com>
2020-08-14 12:29:04 +0900

    DBのスキーマを定義

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE user (
+    `user_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(32) NOT NULL,
+    `created` TIMESTAMP NOT NULL,
+    PRIMARY KEY (user_id),
+    UNIQUE KEY (name),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE diary (
+    `diary_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `name` VARCHAR(32) NOT NULL,
+    `created` TIMESTAMP NOT NULL,
+    PRIMARY KEY (diary_id),
+    UNIQUE KEY (name),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE entry (
+    `entry_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `diary_id` BIGINT UNSIGNED NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
+    `body` VARCHAR(100000) NOT NULL,
+    `created` TIMESTAMP NOT NULL,
+    `updated` TIMESTAMP NOT NULL,
+    PRIMARY KEY (entry_id),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
